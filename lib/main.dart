396d2c4adb4a051d455baefcfcf57bd32dae8916
Shechy Torres <shechytorres@gmail.com>
2021-05-11 02:39:19 -0500

    Icono de facebook agregado

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp('Mi Proyecto'))
+void main(){
+  runApp(MyApp());
+  }
+
 
 class MyApp extends StatelessWidget{
-  String titulo;
-  MyApp(String titulo);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: this.titulo, 
+      title: 'MyApp', 
       theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -20,20 +22,26 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppProyect', 
-        style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.w400))),
+        title: Center(
+          child: Text('AppProyect', 
+            style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.w400)))),
       body: Center(
         child: Column(
           children: [
-            Text('Bienvenido a la AppProyect', style: TextStyle(color: Colors.red)),
+            Text('Bienvenido a la AppProyect',
+              style: TextStyle(color: Colors.red, fontSize: 35.0, fontStyle: FontStyle.italic)),
             Text('En este programa crearemos la AppProyect',
-              style: TextStyle(color: Colors.green, fontSize: 30.0, fontStyle: FontStyle.italic)),
+              style: TextStyle(color: Colors.green, fontSize: 28.0)),
             Row(children: [
               Icon(Icons.ac_unit, size: 30.0, color: Colors.lightBlueAccent),
               Icon(Icons.access_alarm, size: 30.0, color: Colors.yellow),
               Icon(Icons.audiotrack, size: 30.0, color: Colors.blue),
-              Icon(Icons.favorite, size: 30.0, color: Colors.pink)],
-            )
+              Icon(Icons.favorite, size: 30.0, color: Colors.pink),
+              Image.asset(
+                'imagen/facebook-icon.png',
+                height: 30.0,
+              ),
+            ]),
           ],
         )));
   }
